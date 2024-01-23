7da4500a2abb0c8ede2e0eef9ef4666ab48e9237
A00597106 <A00597106@T240-011>
2024-01-23 10:23:05 -0600

    Add project files.

---
+++ Lab1.c.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "I remember the night at the hotel." << endl;
+    cout << "We we're riding in the car when we both fell." << endl;
+    cout << "The words that we whispered." << endl;
+    return 0;
