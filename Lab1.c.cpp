3764fbe337a62757e40af9f74cfa8badc8241eef
Celeste/Talon <105741020+AbidingPython26@users.noreply.github.com>
2024-01-23 10:37:42 -0600

    Update Lab1.c.cpp
    Added a semicolon (;) at the end of line 8

--- Lab1.c.cpp
+++ Lab1.c.cpp
@@ -5,5 +5,5 @@ using namespace std;
 int main() {
     cout << "I remember the night at the hotel." << endl;
     cout << "We we're riding in the car when we both fell." << endl;
-    cout << "The words that we whispered." << endl
+    cout << "The words that we whispered." << endl;
     return 0;
